212343902aad54e70b3a1d00b57801fa8df874fc
Randy Bryant <Randy.Bryant@cs.cmu.edu>
2023-12-13 07:56:38 -0500

    Benchmarks

--- tests/majority/gen.sh
+++ tests/majority/gen.sh
@@ -1,47 +1,97 @@
-!/bin/sh
+#!/bin/sh
 
-make cmaj-sc N=0011
-make cmaj-sc N=0021
-make cmaj-sc N=0031
-make cmaj-sc N=0041
-make cmaj-sc N=0051
-make cmaj-sc N=0061
-make cmaj-sc N=0071
-make cmaj-sc N=0081
-make cmaj-sc N=0091
-make cmaj-sc N=0101
-make cmaj-sc N=0131
-make cmaj-sc N=0161
-make cmaj-sc N=0201
-make cmaj-sc N=0251
-make cmaj-sc N=0301
-make cmaj-sc N=0351
-make cmaj-sc N=0401
-make cmaj-sc N=0501
-make cmaj-sc N=0601
-make cmaj-sc N=0701
-make cmaj-sc N=0801
-make cmaj-sc N=0901
-make cmaj-sc N=1001
+make cmaj-ip N=0011
+make cmaj-ip N=0021
+make cmaj-ip N=0031
+make cmaj-ip N=0041
+make cmaj-ip N=0051
+make cmaj-ip N=0061
+make cmaj-ip N=0071
+make cmaj-ip N=0081
+make cmaj-ip N=0091
+make cmaj-ip N=0101
+make cmaj-ip N=0131
+make cmaj-ip N=0161
+make cmaj-ip N=0201
+make cmaj-ip N=0251
+make cmaj-ip N=0301
+make cmaj-ip N=0351
+make cmaj-ip N=0401
+make cmaj-ip N=0501
+make cmaj-ip N=0601
+make cmaj-ip N=0701
+make cmaj-ip N=0801
+make cmaj-ip N=0901
+make cmaj-ip N=1001
 
-make cmaj-sn N=0011
-make cmaj-sn N=0021
-make cmaj-sn N=0031
-make cmaj-sn N=0041
-make cmaj-sn N=0051
-make cmaj-sn N=0061
-make cmaj-sn N=0071
-make cmaj-sn N=0081
-make cmaj-sn N=0091
-make cmaj-sn N=0101
+make cmaj-ip-0011.pbip
+make cmaj-ip-0021.pbip
+make cmaj-ip-0031.pbip
+make cmaj-ip-0041.pbip
+make cmaj-ip-0051.pbip
+make cmaj-ip-0061.pbip
+make cmaj-ip-0071.pbip
+make cmaj-ip-0081.pbip
+make cmaj-ip-0091.pbip
+make cmaj-ip-0101.pbip
+make cmaj-ip-0131.pbip
+make cmaj-ip-0161.pbip
+make cmaj-ip-0201.pbip
+make cmaj-ip-0251.pbip
+make cmaj-ip-0301.pbip
+make cmaj-ip-0351.pbip
+make cmaj-ip-0401.pbip
+make cmaj-ip-0501.pbip
+make cmaj-ip-0601.pbip
+make cmaj-ip-0701.pbip
+make cmaj-ip-0801.pbip
+make cmaj-ip-0901.pbip
+make cmaj-ip-1001.pbip
 
-make cmaj-tz N=0011
-make cmaj-tz N=0021
-make cmaj-tz N=0031
-make cmaj-tz N=0041
-make cmaj-tz N=0051
-make cmaj-tz N=0061
-make cmaj-tz N=0071
-make cmaj-tz N=0081
-make cmaj-tz N=0091
-make cmaj-tz N=0101
+
+
+# make cmaj-sc N=0011
+# make cmaj-sc N=0021
+# make cmaj-sc N=0031
+# make cmaj-sc N=0041
+# make cmaj-sc N=0051
+# make cmaj-sc N=0061
+# make cmaj-sc N=0071
+# make cmaj-sc N=0081
+# make cmaj-sc N=0091
+# make cmaj-sc N=0101
+# make cmaj-sc N=0131
+# make cmaj-sc N=0161
+# make cmaj-sc N=0201
+# make cmaj-sc N=0251
+# make cmaj-sc N=0301
+# make cmaj-sc N=0351
+# make cmaj-sc N=0401
+# make cmaj-sc N=0501
+# make cmaj-sc N=0601
+# make cmaj-sc N=0701
+# make cmaj-sc N=0801
+# make cmaj-sc N=0901
+# make cmaj-sc N=1001
+
+# make cmaj-sn N=0011
+# make cmaj-sn N=0021
+# make cmaj-sn N=0031
+# make cmaj-sn N=0041
+# make cmaj-sn N=0051
+# make cmaj-sn N=0061
+# make cmaj-sn N=0071
+# make cmaj-sn N=0081
+# make cmaj-sn N=0091
+# make cmaj-sn N=0101
+
+# make cmaj-tz N=0011
+# make cmaj-tz N=0021
+# make cmaj-tz N=0031
+# make cmaj-tz N=0041
+# make cmaj-tz N=0051
+# make cmaj-tz N=0061
+# make cmaj-tz N=0071
+# make cmaj-tz N=0081
+# make cmaj-tz N=0091
+# make cmaj-tz N=0101
--- tests/majority/run.sh
+++ tests/majority/run.sh
@@ -1,8 +1,10 @@
-!/bin/sh
+#!/bin/sh
+python3 ../../tools/pbip_drive.py -x -m s -t 1000 -l files-ip.txt
 python3 ../../tools/pbip_drive.py -x -m s -t 1000 -l files-sc.txt
 python3 ../../tools/pbip_drive.py -x -m s -t 1000 -l files-sn.txt
 python3 ../../tools/pbip_drive.py -x -m s -t 1000 -l files-tz.txt
 
+python3 ../../tools/pbip_drive.py -x -m b -t 1000 -l files-ip.txt
 python3 ../../tools/pbip_drive.py -x -m b -t 1000 -l files-sc.txt
 python3 ../../tools/pbip_drive.py -x -m b -t 1000 -l files-sn.txt
 python3 ../../tools/pbip_drive.py -x -m b -t 1000 -l files-tz.txt
